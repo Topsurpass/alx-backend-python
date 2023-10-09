88f139c7f493aa75224350b3997c606478bafbf2
Temitope Olowosuyi <Temitope.Olowosuyi@etranzact.com>
2023-10-09 12:28:14 +0100

    adjusted pycodestyle

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 """Import wait_random from 0-basic_async_syntax.Write a function
-(do not create an async function, use the regular function syntax to do this)
-task_wait_random that takes an integer max_delay and returns a asyncio.Task."""
+(do not create an async function, use the regular function syntax
+to do this) task_wait_random that takes an integer max_delay and
+returns a asyncio.Task."""
 
-wait_random = __import__('0-basic_async_syntax').wait_random
 import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.tasks:
     """The event loop is responsible for coordinating the execution
-    of asynchronous tasks and coroutines.create_task schedules the execution
-    of the wait_random coroutine within the event loop and returns a task object
-    representing that execution."""
+    of asynchronous tasks and coroutines.create_task schedules
+    the execution of the wait_random coroutine within the event
+    loop and returns a task object representing that execution."""
     createLoop = asyncio.get_event_loop()
     theTask = createLoop.create_task(wait_random(max_delay))
     return theTask
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,14 +1,18 @@
 #!/ustr/bin/env python3
-"""Take the code from wait_n and alter it into a new function task_wait_n.
-    The code is nearly identical to wait_n except task_wait_random is being called."""
-task_wait_random = __import__('3-tasks').task_wait_random
+"""Take the code from wait_n and alter it into a new function
+task_wait_n. The code is nearly identical to wait_n except
+task_wait_random is being called."""
+
 import asyncio
 from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """Take the code from wait_n and alter it into a new function task_wait_n.
-    The code is nearly identical to wait_n except task_wait_random is being called."""
+    """Take the code from wait_n and alter it into a new function
+    task_wait_n. The code is nearly identical to wait_n except
+    task_wait_random is being called."""
+
     outputList = [task_wait_random(max_delay) for _ in range(n)]
     result = []
     for task in asyncio.as_completed(outputList):
