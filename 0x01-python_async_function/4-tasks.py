1688315e16d20f4a4fb67947964e65be7cb08558
Temitope Olowosuyi <Temitope.Olowosuyi@etranzact.com>
2023-10-09 12:56:42 +0100

    corrected the shebang

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,19 +1,18 @@
-#!/ustr/bin/env python3
+#!/usr/bin/env python3
 """Take the code from wait_n and alter it into a new function
 task_wait_n. The code is nearly identical to wait_n except
 task_wait_random is being called."""
 
 import asyncio
 from typing import List
-
-
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    '''Run task_wait_random n times.
-    '''
-    result = await asyncio.gather(
-        *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
-    )
+    """Take the code from wait_n and alter it into a new function
+    task_wait_n. The code is nearly identical to wait_n except
+    task_wait_random is being called."""
+
+    outputList = [task_wait_random(max_delay) for _ in range(n)]
+    result = await asyncio.gather(*outputList)
     return sorted(result)
