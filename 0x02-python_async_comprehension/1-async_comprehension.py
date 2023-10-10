ab92460335b486a426185ff908f68114cb466dbe
Olowosuyi Temitope <temitopeabiodun685@gmail.com>
2023-10-11 00:32:44 +0100

    resolved pycodestyle space error

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """
-Import async_generator from the previous task and then
-write a coroutine called async_comprehension that takes
-no arguments. The coroutine will collect 10 random numbers
-using an async comprehensing over async_generator, then
-return the 10 random numbers.
+Import async_generator from the previous task
+and then write a coroutine called async_comprehension
+that takes no arguments. The coroutine will collect 10
+random numbers using an async comprehensing over
+async_generator, then return the 10 random numbers.
 """
 
 from typing import List
@@ -17,4 +17,3 @@ async def async_comprehension() -> List[float]:
     then return the 10 random numbers"""
     result = [gen async for gen in async_generator()]
     return result
-    
