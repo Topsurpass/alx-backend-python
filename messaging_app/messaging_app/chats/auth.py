6d9b3c2ce7743a9323234327530c520c7a754ec1
Olowosuyi Temitope <Temz@ETZ-TOLO-AUD.etranzact.com>
2024-12-24 12:00:24 +0100

    restore file structure to default

--- messaging_app/messaging_app/chats/auth.py
+++
@@ -1,7 +0,0 @@
-from rest_framework_simplejwt.views import TokenObtainPairView
-
-class CustomTokenObtainPairView(TokenObtainPairView):
-    """
-    Custom view for obtaining JWT tokens.
-    """
-    pass
